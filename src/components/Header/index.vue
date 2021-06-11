521e0a33e19af0837798d4bbade447c919159766
Kamil Szerląg <kamilszerlag@gmail.com>
2021-06-11 19:38:54 +0200

    Create Header

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -3,6 +3,9 @@
     <div class="header__title-container">
       <h1 class="header__title">The Planets</h1>
       <p class="header__description">Find your favourite planet.</p>
+      <div class="header__cta-container">
+        <a class="header__cta" href="#planets">Let's start</a>
+      </div>
     </div>
     <div class="header__image-container">
       <img class="header__image" :src="image" />
