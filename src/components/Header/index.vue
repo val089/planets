402b38783ffe87a2780c8f9b3bc3a5b9662f3027
Kamil Szerląg <kamilszerlag@gmail.com>
2021-06-11 19:04:19 +0200

    Create Header

---
+++ src/components/Header/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="header">
+    <div class="header__title-container">
+      <h1 class="header__title">The Planets</h1>
+      <p class="header__description">Find your favourite planet.</p>
+    </div>
+    <div class="header__image-container">
+      <img class="header__image" :src="image" />
+    </div>
+  </header>
+</template>
+<script src="./component.js"></script>
+<style src="./style.scss" lang="scss" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="home">
+    <Header />
     <PlanetsList />
   </div>
 </template>
 
 <script>
+import Header from '@/components/Header';
 import PlanetsList from '@/components/PlanetsList';
 
 export default {
   name: 'Home',
   components: {
+    Header,
     PlanetsList,
   },
 };
