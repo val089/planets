c4462bd7033ae1e2f14e01ccc87ecf4c61e6c853
Kamil Szerląg <kamilszerlag@gmail.com>
2021-06-11 18:55:13 +0200

    Create Spinner

---
+++ src/components/Spinner/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="loading">
+    <div class="lds-ring">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+</template>
+<script src="./component.js" />
+<style src="./style.scss" lang="scss" scoped></style>
